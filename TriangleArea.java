6d6f0e28660b2b7469fea28c05619cfd1b0dbcde
Olga <olichka99@yahoo.com>
2016-03-24 15:14:29 +0200

    Added files via upload

---
+++ TriangleArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        int aX,aY,bX,bY,cX,cY, area;
+
+        Scanner input = new Scanner(System.in);
+        aX = input.nextInt();
+        aY = input.nextInt();
+        bX = input.nextInt();
+        bY = input.nextInt();
+        cX = input.nextInt();
+        cY = input.nextInt();
+
+        area = (aX * (bY-cY) +  bX * (cY-aY) + cX * (aY-bY))/2;
+
+                System.out.println(Math.abs(area));
+    }
+}
